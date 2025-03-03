a1d7be24cfb11d3d48ce1fa5d2ab3299b1b3baab
Cauterization <71918928+Cauterization@users.noreply.github.com>
2025-03-03 13:57:33 +0300

    refactor: add helper functions

--- demo/midgard-node/src/database/transaction.ts
+++ demo/midgard-node/src/database/transaction.ts
@@ -3,143 +3,109 @@ import sqlite3 from "sqlite3";
 import { logAbort, logInfo } from "../utils.js";
 import { utxoToNormalizedAssets, utxoToRow } from "./utils.js";
 
+const runAsync = (
+  db: sqlite3.Database,
+  query: string,
+  params?: any[],
+  rejectCallback?: (err: Error) => void,
+  resolveCallback?: (res: sqlite3.RunResult) => void
+) => {
+  return new Promise<void>((resolve, reject) => {
+    db.run(query, params, function (err) {
+      if (err) {
+        if (rejectCallback) rejectCallback(err);
+        else logAbort(`db: an error occurred: ${err}`);
+        reject(err);
+      } else {
+        if (resolveCallback) resolveCallback(this);
+        resolve();
+      }
+    });
+  });
+};
+
+const runBeginTransaction = async (db: sqlite3.Database, funcName: string) => {
+  runAsync(db, "BEGIN TRANSACTION", undefined, (err) =>
+    logAbort(`db: error beginning transaction from ${funcName}: ${err.message}`)
+  );
+};
+
+const runCommitTransaction = async (db: sqlite3.Database, funcName: string) => {
+  runAsync(db, "COMMIT", undefined, (err) =>
+    logAbort(
+      `db: error committing transaction from ${funcName}: ${err.message}`
+    )
+  );
+};
+
+const runRollbackTransaction = async (
+  db: sqlite3.Database,
+  funcName: string,
+  err: any
+) => {
+  logAbort(`db: an error occurred during transaction: ${err}`);
+  await runAsync(db, "ROLLBACK", undefined, (err) =>
+    logAbort(`db: transaction rollback error from ${funcName}: ${err.message}`)
+  );
+};
+
 export const submitTx = async (
   db: sqlite3.Database,
   txHash: string,
   txCBOR: string,
   spent: OutRef[],
   produced: UTxO[]
-) => {
-  return new Promise<void>(async (resolve, reject) => {
-    try {
-      const normalizedAssets = produced.flatMap((utxo) =>
-        utxoToNormalizedAssets(utxo)
-      );
+): Promise<void> => {
+  try {
+    await runBeginTransaction(db, "submitTx");
 
-      await new Promise<void>((resolve) => {
-        db.run("BEGIN TRANSACTION;", function (err) {
-          if (err) {
-            logAbort(`db: error beggining transaction: ${err.message}`);
-            reject(err);
-          }
-          resolve();
-        });
-      });
+    await runAsync(
+      db,
+      `INSERT INTO mempool (tx_hash, tx_cbor) VALUES (?, ?)`,
+      [fromHex(txHash), fromHex(txCBOR)],
+      (err) => logAbort(`mempool db: error inserting tx: ${err.message}`),
+      (res) => logInfo(`mempool db: tx stored with rowid ${res.lastID}`)
+    );
 
-      const mempoolInsertQuery = `
-      INSERT INTO mempool (tx_hash, tx_cbor) VALUES (?, ?)`;
-      const mempoolInsertValues = [fromHex(txHash), fromHex(txCBOR)];
-      await new Promise<void>((resolve) => {
-        db.run(mempoolInsertQuery, mempoolInsertValues, function (err) {
-          if (err) {
-            logAbort(`mempool db: error inserting tx: ${err.message}`);
-            reject(err);
-          }
-          logInfo(`mempool db: tx stored with rowid ${this.lastID}`);
-          resolve();
-        });
-      });
-
-      const mempoolLedgerDeleteQuery = `
-      DELETE FROM mempool_ledger WHERE (tx_hash, output_index) IN (${spent
+    await runAsync(
+      db,
+      `DELETE FROM mempool_ledger WHERE (tx_hash, output_index) IN (${spent
         .map(() => `(?, ?)`)
-        .join(", ")})`;
-      const mempoolLedgerDeleteValues = spent.flatMap((r) => [
-        fromHex(r.txHash),
-        r.outputIndex,
-      ]);
-      await new Promise<void>((resolve) => {
-        db.run(
-          mempoolLedgerDeleteQuery,
-          mempoolLedgerDeleteValues,
-          function (err) {
-            if (err) {
-              logAbort(
-                `mempool_ledger db: utxos removing error: ${err.message}`
-              );
-              reject(err);
-            }
-            logInfo(`mempool_ledger db: ${this.changes} UTXOs removed`);
-            resolve();
-          }
-        );
-      });
+        .join(", ")})`,
+      spent.flatMap((r) => [fromHex(r.txHash), r.outputIndex]),
+      (err) =>
+        logAbort(`mempool_ledger db: utxos removing error: ${err.message}`),
+      (res) => logInfo(`mempool_ledger db: ${res.changes} UTXOs removed`)
+    );
 
-      const mempoolLedgerInsertQuery = `
-        INSERT INTO mempool_ledger
-            (tx_hash, output_index, address, datum_hash, datum, script_ref_type, script_ref_script)
-          VALUES ${produced.map(() => `(?, ?, ?, ?, ?, ?, ?)`).join(", ")};
-          `;
-      const mempoolLedgerInsertValues = produced.flatMap((utxo) =>
-        Object.values(utxoToRow(utxo))
-      );
-      await new Promise<void>((resolve) => {
-        db.run(
-          mempoolLedgerInsertQuery,
-          mempoolLedgerInsertValues,
-          function (err) {
-            if (err) {
-              logAbort(
-                `mempool_ledger db: error inserting UTXOs: ${err.message}`
-              );
-              reject(err);
-            }
-            logInfo(`mempool_ledger db: ${produced.length} new UTXOs added`);
-            resolve();
-          }
-        );
-      });
+    await runAsync(
+      db,
+      `INSERT INTO mempool_ledger
+        (tx_hash, output_index, address, datum_hash, datum, script_ref_type, script_ref_script)
+        VALUES ${produced.map(() => `(?, ?, ?, ?, ?, ?, ?)`).join(", ")}`,
+      produced.flatMap((utxo) => Object.values(utxoToRow(utxo))),
+      (err) =>
+        logAbort(`mempool_ledger db: error inserting UTXOs: ${err.message}`),
+      (_) => logInfo(`mempool_ledger db: ${produced.length} new UTXOs added`)
+    );
 
-      const mempoolLedgerInsertAssetsQuery = `
-      INSERT INTO mempool_ledger_assets (tx_hash, output_index, unit, quantity)
-        VALUES ${normalizedAssets.map(() => `(?, ?, ?, ?)`).join(", ")}
-        `;
-      const mempoolLedgerInsertAssetsValues = normalizedAssets.flatMap((v) =>
-        Object.values(v)
-      );
-      await new Promise<void>((resolve) => {
-        db.run(
-          mempoolLedgerInsertAssetsQuery,
-          mempoolLedgerInsertAssetsValues,
-          function (err) {
-            if (err) {
-              logAbort(
-                `mempool_ledger db: error inserting assets: ${err.message}`
-              );
-              reject(err);
-            }
-            logInfo(
-              `mempool_ledger db: ${normalizedAssets.length} assets added`
-            );
-            resolve();
-          }
-        );
-      });
+    const normalizedAssets = produced.flatMap((utxo) =>
+      utxoToNormalizedAssets(utxo)
+    );
+    await runAsync(
+      db,
+      `INSERT INTO mempool_ledger_assets (tx_hash, output_index, unit, quantity)
+        VALUES ${normalizedAssets.map(() => `(?, ?, ?, ?)`).join(", ")}`,
+      normalizedAssets.flatMap((v) => Object.values(v)),
+      (err) =>
+        logAbort(`mempool_ledger db: error inserting assets: ${err.message}`),
+      (res) => logInfo(`mempool_ledger db: ${res.changes} assets added`)
+    );
 
-      await new Promise<void>((resolve) => {
-        db.run("COMMIT;", function (err) {
-          if (err) {
-            logAbort(`db: error commiting transaction: ${err.message}`);
-            reject(err);
-          }
-          resolve();
-        });
-      });
-
-      resolve();
-    } catch (err) {
-      await new Promise<void>((resolve) => {
-        db.run("ROLLBACK;", function (err) {
-          if (err) {
-            logAbort(`db: transaction rollback error: ${err.message}`);
-            reject(err);
-          }
-          resolve();
-        });
-      });
-      throw err;
-    }
-  });
+    await runCommitTransaction(db, "submitTx");
+  } catch (err) {
+    runRollbackTransaction(db, "submitTx", err);
+  }
 };
 
 export const submitBlock = async (
@@ -148,150 +114,80 @@ export const submitBlock = async (
   produced: UTxO[],
   txs: { txHash: string; txCbor: string }[]
 ) => {
-  return new Promise<void>(async (resolve, reject) => {
-    try {
-      const normalizedAssets = produced.flatMap((utxo) =>
-        utxoToNormalizedAssets(utxo)
-      );
+  try {
+    const normalizedAssets = produced.flatMap((utxo) =>
+      utxoToNormalizedAssets(utxo)
+    );
 
-      await new Promise<void>((resolve) => {
-        db.run("BEGIN TRANSACTION;", function (err) {
+    await runBeginTransaction(db, "submitBlock");
+
+    await runAsync(
+      db,
+      `DELETE FROM latest_ledger WHERE (tx_hash, output_index) IN (${spent
+        .map(() => `(?, ?)`)
+        .join(", ")})`,
+      spent.flatMap((r) => [fromHex(r.txHash), r.outputIndex]),
+      (err) =>
+        logAbort(`latest_ledger db: utxos removing error: ${err.message}`)
+    );
+
+    await runAsync(
+      db,
+      `INSERT INTO latest_ledger
+          (tx_hash, output_index, address, datum_hash, datum, script_ref_type, script_ref_script)
+          VALUES ${produced.map(() => `(?, ?, ?, ?, ?, ?, ?)`).join(", ")}`,
+      produced.flatMap((utxo) => Object.values(utxoToRow(utxo))),
+      (err) =>
+        logAbort(`latest_ledger db: error inserting UTXOs: ${err.message}`),
+      (_) => logInfo(`latest_ledger db: ${produced.length} new UTXOs added`)
+    );
+
+    await runAsync(
+      db,
+      `INSERT INTO latest_ledger_assets (tx_hash, output_index, unit, quantity)
+          VALUES ${normalizedAssets.map(() => `(?, ?, ?, ?)`).join(", ")}`,
+      normalizedAssets.flatMap((v) => Object.values(v)),
+      (err) =>
+        logAbort(`latest_ledger db: error inserting assets: ${err.message}`),
+      (res) => logInfo(`latest_ledger db: ${res.changes} assets added`)
+    );
+
+    await runAsync(
+      db,
+      `DELETE FROM mempool`,
+      [],
+      (err) => logAbort(`mempool db: clearing error: ${err.message}`),
+      (_) => logInfo(`mempool db: cleared`)
+    );
+
+    await new Promise<void>((resolve, reject) => {
+      const query = `INSERT INTO immutable (tx_hash, tx_cbor) VALUES (?, ?)`;
+      const stmt = db.prepare(query);
+
+      for (const { txHash, txCbor } of txs) {
+        stmt.run([fromHex(txHash), fromHex(txCbor)], function (err) {
           if (err) {
-            logAbort(`db: error beggining transaction: ${err.message}`);
+            logAbort(`immutable db: error inserting tx: ${err.message}`);
             reject(err);
+            return;
           }
-          resolve();
+          logInfo(`immutable db: tx stored with rowid ${this.lastID}`);
         });
-      });
+      }
 
-      await new Promise<void>((resolve) => {
-        const latestLedgerDeleteQuery = `
-          DELETE FROM latest_ledger WHERE (tx_hash, output_index) IN (${spent
-            .map(() => `(?, ?)`)
-            .join(", ")});`;
-        const latestLedgerDeleteValues = spent.flatMap((r) => [
-          fromHex(r.txHash),
-          r.outputIndex,
-        ]);
-        db.run(latestLedgerDeleteQuery, latestLedgerDeleteValues, (err) => {
-          if (err) {
-            logAbort(`latest_ledger db: utxos removing error: ${err.message}`);
-            reject(err);
-          }
+      stmt.finalize((err) => {
+        if (err) {
+          reject(err);
+        } else {
           resolve();
-        });
-      });
-
-      await new Promise<void>((resolve) => {
-        const latestLedgerInsertQuery = `
-          INSERT INTO latest_ledger
-              (tx_hash, output_index, address, datum_hash, datum, script_ref_type, script_ref_script)
-            VALUES ${produced.map(() => `(?, ?, ?, ?, ?, ?, ?)`).join(", ")};
-            `;
-        const latestLedgerInsertValues = produced.flatMap((utxo) =>
-          Object.values(utxoToRow(utxo))
-        );
-        db.run(
-          latestLedgerInsertQuery,
-          latestLedgerInsertValues,
-          function (err) {
-            if (err) {
-              logAbort(
-                `latest_ledger db: error inserting UTXOs: ${err.message}`
-              );
-              reject(err);
-            }
-            logInfo(`latest_ledger db: ${produced.length} new UTXOs added`);
-            resolve();
-          }
-        );
-      });
-
-      const latestLedgerInsertAssetsQuery = `
-      INSERT INTO latest_ledger_assets (tx_hash, output_index, unit, quantity)
-        VALUES ${normalizedAssets.map(() => `(?, ?, ?, ?)`).join(", ")}
-        `;
-      const latestLedgerInsertAssetsValues = normalizedAssets.flatMap((v) =>
-        Object.values(v)
-      );
-      await new Promise<void>((resolve) => {
-        db.run(
-          latestLedgerInsertAssetsQuery,
-          latestLedgerInsertAssetsValues,
-          function (err) {
-            if (err) {
-              logAbort(
-                `latest_ledger db: error inserting assets: ${err.message}`
-              );
-              reject(err);
-            }
-            logInfo(
-              `latest_ledger db: ${normalizedAssets.length} assets added`
-            );
-            resolve();
-          }
-        );
-      });
-
-      await new Promise<void>((resolve) => {
-        db.run(`DELETE FROM mempool;`, function (err) {
-          if (err) {
-            logAbort(`mempool db: clearing error: ${err.message}`);
-            reject(err);
-          }
-          logInfo(`mempool db: cleared`);
-          resolve();
-        });
-      });
-
-      await new Promise<void>((resolve, reject) => {
-        const query = `INSERT INTO immutable (tx_hash, tx_cbor) VALUES (?, ?)`;
-        const stmt = db.prepare(query);
-
-        for (const { txHash, txCbor } of txs) {
-          stmt.run([fromHex(txHash), fromHex(txCbor)], function (err) {
-            if (err) {
-              logAbort(`immutable db: error inserting tx: ${err.message}`);
-              reject(err);
-              return;
-            }
-            logInfo(`immutable db: tx stored with rowid ${this.lastID}`);
-          });
         }
-
-        stmt.finalize((err) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve();
-          }
-        });
       });
+    });
 
-      await new Promise<void>((resolve) => {
-        db.run("COMMIT;", function (err) {
-          if (err) {
-            logAbort(`db: error commiting transaction: ${err.message}`);
-            reject(err);
-          }
-          resolve();
-        });
-      });
-
-      resolve();
-    } catch (err) {
-      await new Promise<void>((resolve) => {
-        db.run("ROLLBACK;", function (err) {
-          if (err) {
-            logAbort(`db: transaction rollback error: ${err.message}`);
-            reject(err);
-          }
-          resolve();
-        });
-      });
-      throw err;
-    }
-  });
+    await runCommitTransaction(db, "submitBlock");
+  } catch (err) {
+    runRollbackTransaction(db, "submitBlock", err);
+  }
 };
 
 export const buildAndSubmitMergeTx = async (
@@ -300,114 +196,50 @@ export const buildAndSubmitMergeTx = async (
   producedUTxOs: UTxO[],
   headerHash: string
 ) => {
-  return new Promise<void>(async (resolve, reject) => {
-    try {
-      await new Promise<void>((resolve) => {
-        db.run("BEGIN TRANSACTION;", function (err) {
-          if (err) {
-            logAbort(`db: error beggining transaction: ${err.message}`);
-            reject(err);
-          }
-          resolve();
-        });
-      });
+  try {
+    await runBeginTransaction(db, "buildAndSubmitMergeTx");
 
-      await new Promise<void>((resolve) => {
-        const query = `DELETE FROM confirmed_ledger WHERE (tx_hash, output_index) IN (${spentOutRefs
-          .map(() => `(?, ?)`)
-          .join(", ")})`;
-        const values = spentOutRefs.flatMap((r) => [
-          fromHex(r.txHash),
-          r.outputIndex,
-        ]);
-        db.run(query, values, function (err) {
-          if (err) {
-            logAbort(
-              `confirmed_ledger db: error clearing utxos: ${err.message}`
-            );
-            reject(err);
-          }
-          resolve();
-        });
-      });
+    await runAsync(
+      db,
+      `DELETE FROM confirmed_ledger WHERE (tx_hash, output_index) IN (${spentOutRefs
+        .map(() => `(?, ?)`)
+        .join(", ")})`,
+      spentOutRefs.flatMap((r) => [fromHex(r.txHash), r.outputIndex]),
+      (err) =>
+        logAbort(`confirmed_ledger db: error clearing utxos: ${err.message}`)
+    );
 
-      await new Promise<void>((resolve) => {
-        const query = `
-          INSERT INTO confirmed_ledger
-              (tx_hash, output_index, address, datum_hash, datum, script_ref_type, script_ref_script)
-            VALUES
-          ${producedUTxOs.map(() => `(?, ?, ?, ?, ?, ?, ?)`).join(", ")}
-          `;
-        const values = producedUTxOs.flatMap((utxo) =>
-          Object.values(utxoToRow(utxo))
-        );
-        db.run(query, values, function (err) {
-          if (err) {
-            logAbort(
-              `confirmed_ledger db: error inserting utxos: ${err.message}`
-            );
-            reject(err);
-          }
-          resolve();
-        });
-      });
+    await runAsync(
+      db,
+      `INSERT INTO confirmed_ledger
+          (tx_hash, output_index, address, datum_hash, datum, script_ref_type, script_ref_script)
+          VALUES ${producedUTxOs.map(() => `(?, ?, ?, ?, ?, ?, ?)`).join(", ")}`,
+      producedUTxOs.flatMap((utxo) => Object.values(utxoToRow(utxo))),
+      (err) =>
+        logAbort(`confirmed_ledger db: error inserting utxos: ${err.message}`)
+    );
 
-      await new Promise<void>((resolve) => {
-        const normalizedAssets = producedUTxOs.flatMap((utxo) =>
-          utxoToNormalizedAssets(utxo)
-        );
-        const query = `
-          INSERT INTO confirmed_ledger_assets
-              (tx_hash, output_index, unit, quantity)
-            VALUES
-              ${normalizedAssets.map(() => `(?, ?, ?, ?)`).join(", ")}
-          `;
-        const values = normalizedAssets.flatMap((v) => Object.values(v));
-        db.run(query, values, function (err) {
-          if (err) {
-            logAbort(
-              `confirmed_ledger db: error inserting assets: ${err.message}`
-            );
-            reject(err);
-          }
-          resolve();
-        });
-      });
+    const normalizedAssets = producedUTxOs.flatMap((utxo) =>
+      utxoToNormalizedAssets(utxo)
+    );
+    await runAsync(
+      db,
+      `INSERT INTO confirmed_ledger_assets (tx_hash, output_index, unit, quantity)
+          VALUES ${normalizedAssets.map(() => `(?, ?, ?, ?)`).join(", ")}`,
+      normalizedAssets.flatMap((v) => Object.values(v)),
+      (err) =>
+        logAbort(`confirmed_ledger db: error inserting assets: ${err.message}`)
+    );
 
-      await new Promise<void>((resolve) => {
-        const query = `DELETE from blocks WHERE header_hash = ?`;
-        db.run(query, [fromHex(headerHash)], function (err) {
-          if (err) {
-            logAbort(`blocks db: clearing error: ${err.message}`);
-            reject(err);
-          } else {
-            resolve();
-          }
-        });
-      });
+    await runAsync(
+      db,
+      `DELETE from blocks WHERE header_hash = ?`,
+      [fromHex(headerHash)],
+      (err) => logAbort(`blocks db: clearing error: ${err.message}`)
+    );
 
-      await new Promise<void>((resolve) => {
-        db.run("COMMIT;", function (err) {
-          if (err) {
-            logAbort(`db: error commiting transaction: ${err.message}`);
-            reject(err);
-          }
-          resolve();
-        });
-      });
-
-      resolve();
-    } catch (err) {
-      await new Promise<void>((resolve) => {
-        db.run("ROLLBACK;", function (err) {
-          if (err) {
-            logAbort(`db: transaction rollback error: ${err.message}`);
-            reject(err);
-          }
-          resolve();
-        });
-      });
-      throw err;
-    }
-  });
+    await runCommitTransaction(db, "buildAndSubmitMergeTx");
+  } catch (err) {
+    await runRollbackTransaction(db, "buildAndSubmitMergeTx", err);
+  }
 };
