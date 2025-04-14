8e5f3e538c9969aa23b307bdfd328b02c2c80385
Cauterization <71918928+Cauterization@users.noreply.github.com>
2025-04-14 18:57:09 +0300

    refactor: remove redundant code

--- onchain/plutarch/app/Main.hs
+++ onchain/plutarch/app/Main.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 {-# LANGUAGE ImpredicativeTypes #-}
 
 module Main (main) where
@@ -14,31 +11,26 @@ import Data.Bifunctor (
  )
 import Data.ByteString.Base16 qualified as Base16
 import Data.ByteString.Lazy qualified as LBS
-import Data.Default (def)
 import Data.Text (
   Text,
   pack,
  )
 import Data.Text.Encoding qualified as Text
 import Plutarch (
-  Config (..),
-  TracingMode (..),
-  LogLevel (..), 
+  Config (..), 
   compile,
-  printScript,
  )
 import Plutarch.Evaluate (
   evalScript,
   applyArguments
  )
 import Plutarch.LedgerApi.V3 (PScriptContext)
-import Plutarch.Prelude
+import Plutarch.Prelude ( type (:-->), ClosedTerm, PUnit )
 import Plutarch.Script (Script, serialiseScript)
 import PlutusLedgerApi.V2 (
   Data,
   ExBudget,
  )
-import System.IO
 
 encodeSerialiseCBOR :: Script -> Text
 encodeSerialiseCBOR = Text.decodeUtf8 . Base16.encode . CBOR.serialize' . serialiseScript
@@ -53,21 +45,10 @@ evalWithArgsT cfg x args = do
   scr <- first (pack . show) escr
   pure (scr, budg, trc)
 
--- writePlutusScript :: String -> FilePath -> ClosedTerm a -> IO ()
--- writePlutusScript title filepath term = do
---   case evalT term of
---     Left e -> putStrLn (show e)
---     Right (script, _, _) -> do
---       let
---         scriptType = "PlutusScriptV2" :: String
---         plutusJson = object ["type" .= scriptType, "description" .= title, "cborHex" .= encodeSerialiseCBOR script]
---         content = encodePretty plutusJson
---       LBS.writeFile filepath content
-
 writePlutusScript :: Config -> String -> FilePath -> ClosedTerm a -> IO ()
 writePlutusScript cfg title filepath term = do
   case evalT cfg term of
-    Left e -> putStrLn (show e)
+    Left e -> print e
     Right (script, _, _) -> do
       let
         scriptType = "PlutusScriptV2" :: String
@@ -75,13 +56,13 @@ writePlutusScript cfg title filepath term = do
         content = encodePretty plutusJson
       LBS.writeFile filepath content
 
-writePlutusScriptTraceBind :: String -> FilePath -> ClosedTerm a -> IO ()
-writePlutusScriptTraceBind title filepath term =
-  writePlutusScript (Tracing LogInfo DoTracingAndBinds) title filepath term
+-- writePlutusScriptTraceBind :: String -> FilePath -> ClosedTerm a -> IO ()
+-- writePlutusScriptTraceBind title filepath term =
+--   writePlutusScript (Tracing LogInfo DoTracingAndBinds) title filepath term
 
-writePlutusScriptTrace :: String -> FilePath -> ClosedTerm a -> IO ()
-writePlutusScriptTrace title filepath term =
-  writePlutusScript (Tracing LogInfo DoTracing) title filepath term
+-- writePlutusScriptTrace :: String -> FilePath -> ClosedTerm a -> IO ()
+-- writePlutusScriptTrace title filepath term =
+--   writePlutusScript (Tracing LogInfo DoTracing) title filepath term
 
 writePlutusScriptNoTrace :: String -> FilePath -> ClosedTerm a -> IO ()
 writePlutusScriptNoTrace title filepath term =
