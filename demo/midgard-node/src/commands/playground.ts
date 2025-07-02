ec60ff09aa9da877ee0b378920b9ab87c50ef43b
keyanmaskoot <keyanmaskoot@gmail.com>
2025-07-02 11:17:34 +0200

    refactor: remove extra services from docker

--- demo/midgard-node/src/commands/playground.ts
+++ demo/midgard-node/src/commands/playground.ts
@@ -1,5 +1,5 @@
 import { NodeConfig, User } from "@/config.js";
-import { fromHex } from "@lucid-evolution/lucid";
+import { fromHex, toHex } from "@lucid-evolution/lucid";
 import {
   Effect,
   pipe,
@@ -20,7 +20,7 @@ const playgroundFork =
     const trie = yield* Effect.tryPromise({
       try: () =>
         ETH.createMPT({
-          db,
+          db: db.db,
           useRootPersistence: true,
           valueEncoding: ETH_UTILS.ValueEncoding.Bytes,
         }),
@@ -30,8 +30,60 @@ const playgroundFork =
       try: () => trie.put(fromHex("00"), fromHex("c0ffee")),
       catch: (e) => new Error(`${e}`),
     });
+    yield* Effect.tryPromise({
+      try: () => trie.put(fromHex("01"), fromHex("c0ffef")),
+      catch: (e) => new Error(`${e}`),
+    });
+    trie.checkpoint();
+    const allRowsBeforeEffects = yield* db.getAllEffect()
+    yield* Effect.logInfo(
+      allRowsBeforeEffects.map(
+        ({key, value}) => ({key: toHex(key), value: toHex(value)})
+      )
+    );
+    yield* Effect.all([
+      Effect.tryPromise({
+        try: () => trie.put(fromHex("02"), fromHex("c0fff0")),
+        catch: (e) => new Error(`${e}`),
+      }),
+      Effect.tryPromise({
+        try: () => trie.put(fromHex("03"), fromHex("c0fff1")),
+        catch: (e) => new Error(`${e}`),
+      }),
+      Effect.tryPromise({
+        try: () => trie.del(fromHex("04")),
+        catch: (e) => new Error(`${e}`),
+      }),
+      Effect.tryPromise({
+        try: trie.commit,
+        catch: (e) => new Error(`${e}`),
+      }),
+    ], { concurrency: 1 }).pipe(
+      Effect.catchAll(
+        (_e) => Effect.gen(function* () {
+          yield* Effect.logInfo("REVERTING...");
+          yield* Effect.tryPromise({
+            try: trie.revert,
+            catch: (e) => new Error(`${e}`)
+          });
+        }),
+      )
+    );
+    const val0 = yield* Effect.tryPromise({
+      try: () => trie.get(fromHex("00")),
+      catch: (e) => new Error(`${e}`),
+    });
     const allRows = yield* db.getAllEffect()
-    yield* Effect.logInfo(allRows)
+    if (val0) {
+      yield* Effect.logInfo(toHex(val0));
+    } else {
+      yield* Effect.logInfo("VALUE OF KEY \"00\" NOT FOUND.");
+    }
+    yield* Effect.logInfo(
+      allRows.map(
+        ({key, value}) => ({key: toHex(key), value: toHex(value)})
+      )
+    );
   });
 
 const program = pipe(
