6213dc8175fdd0cde5c6850b874cb802f3cc09d9
Georgii Gerasev <george30032002@gmail.com>
2025-10-02 15:57:50 +0400

    feat: cherry picked types from the deposit-tx-builder

--- demo/midgard-sdk/src/tx-builder/user-events/deposit/types.ts
+++ demo/midgard-sdk/src/tx-builder/user-events/deposit/types.ts
@@ -1,22 +1,39 @@
-import { Address, PolicyId, Data, UTxO } from "@lucid-evolution/lucid";
-import { POSIXTimeSchema } from "@/tx-builder/common.js";
+import { Data } from "@lucid-evolution/lucid";
+import { OutputReferenceSchema, POSIXTimeSchema } from "@/tx-builder/common.js";
 
-export const DepositDatumSchema = Data.Object({
-  data: Data.Object({
-    startTime: POSIXTimeSchema,
-    endTime: POSIXTimeSchema,
-  }),
+export const DepositEventSchema = Data.Object({
+    id: OutputReferenceSchema,
+    info: Data.Object({
+      l2_address: Data.Bytes(),
+      l2_datum: Data.Nullable(Data.Bytes())
+    })
 });
 
-export type Datum = Data.Static<typeof DepositDatumSchema>;
-export const Datum = DepositDatumSchema as unknown as Datum;
+export type DepositEvent = Data.Static<typeof DepositEventSchema>;
+export const DepositEvent = DepositEventSchema as unknown as DepositEvent;
 
-export type DepositUTxO = {
-  utxo: UTxO;
-  datum: Datum;
-};
+export const DepositInfoSchema = Data.Object({
+    l2_address: Data.Bytes(),
+    l2_datum: Data.Nullable(Data.Bytes())
+});
 
-export type FetchConfig = {
-  depositAddress: Address;
-  depositPolicyId: PolicyId;
-};
+export type DepositInfo = Data.Static<typeof DepositInfoSchema>;
+export const DepositInfo = DepositInfoSchema as unknown as DepositInfo;
+
+export const DepositDatumSchema = Data.Object({
+    event: DepositEventSchema,
+    inclusion_time: POSIXTimeSchema, // inclusion time is important , time range , 
+    })
+
+export type DepositDatum = Data.Static<typeof DepositDatumSchema>;
+export const DepositDatum = DepositDatumSchema as unknown as DepositDatum;
+
+export const MintRedeemerSchema = Data.Enum([
+     Data.Object({ AuthenticateEvent: Data.Object({ nonce_input_index: Data.Integer(), event_output_index: Data.Integer(),
+                                         hub_ref_input_index: Data.Integer(),witness_registration_redeemer_index:Data.Integer()}) 
+                }),
+     Data.Object({ BurnEventNFT: Data.Object({ nonce_asset_name: Data.Bytes(), witness_unregistration_redeemer_index: Data.Integer() }) })           
+]);
+
+export type MintRedeemer = Data.Static<typeof MintRedeemerSchema>;
+export const MintRedeemer = MintRedeemerSchema as unknown as MintRedeemer;
