445a7ca5c4384b5216ef8aadeb1c17b7554d5482
Suganya Arun <realsuganya01@gmail.com>
2025-10-07 14:08:49 -0400

    fix: redeemer builder and errortype

--- demo/midgard-sdk/src/tx-builder/user-events/deposit/index.ts
+++ demo/midgard-sdk/src/tx-builder/user-events/deposit/index.ts
@@ -1,9 +1,8 @@
-import { Data, Integer, LucidEvolution, Script, toUnit, TxBuilder} from "@lucid-evolution/lucid";
+import { Data, Integer, LucidEvolution, RedeemerBuilder, Script, toUnit, TransactionError, TxBuilder} from "@lucid-evolution/lucid";
 import { Datum, DepositInfo, MintRedeemer } from "./types.js";
 import { POSIXTime } from "@/tx-builder/common.js";
 import { bufferToHex, hashHexWithBlake2b256 } from "@/utils/common.js";
 import { Effect } from "effect";
-import { HashingError, LucidError } from "@/utils/common.js";
 
 export type DepositParams = {
   depositScriptAddress: string,
@@ -24,31 +23,40 @@ export type DepositParams = {
 export const depositTxBuilder = (
   lucid: LucidEvolution,
   params: DepositParams,
-): Effect.Effect<TxBuilder, HashingError | LucidError > =>
+): Effect.Effect<TxBuilder, TransactionError | unknown > =>
   Effect.gen(function* () {
 
-  const redeemer: MintRedeemer = {
-    AuthenticateEvent: {
-        nonce_input_index: 0n,
-        event_output_index: 0n,
-        hub_ref_input_index: 0n,
-        witness_registration_redeemer_index: 0n
-    }
-  };
-  const authenticateEvent = Data.to(redeemer,MintRedeemer);
   const utxos = yield* Effect.promise(() => lucid.wallet().getUtxos());
   if (utxos.length === 0) {
     throw new Error("No UTxOs found in wallet");
   }
   const inputUtxo = utxos[0];
   const assetNameBuffer = yield* hashHexWithBlake2b256(inputUtxo.txHash);
-  const assetName = bufferToHex(Buffer.from(assetNameBuffer));
+  const assetName = bufferToHex(Buffer.from(assetNameBuffer,'hex'));
   const depositNFT = toUnit(params.policyId,assetName);
   const currDatum: Datum = {
   event: {id: { txHash: { hash: inputUtxo.txHash }, outputIndex: BigInt(inputUtxo.outputIndex)}, info : params.depositInfo},
   inclusion_time: params.inclusionTime,
  };
    const depositDatum = Data.to(currDatum, Datum);
+   const authenticateEvent: RedeemerBuilder = {
+    kind: "selected",
+    makeRedeemer: (inputIdxs: bigint[]) => {
+      const tupleList = inputIdxs.map((inputIdx, i) => [inputIdx, BigInt(i)]);
+      
+      const redeemer: MintRedeemer = {
+        AuthenticateEvent: {
+          nonce_input_index: tupleList[0][0],      
+          event_output_index: 0n,       
+          hub_ref_input_index: 0n,                   
+          witness_registration_redeemer_index: 0n    
+        }
+      };
+      
+      return Data.to(redeemer, MintRedeemer);
+    },
+    inputs: [inputUtxo], 
+  };
    const tx = lucid
     .newTx()
     .collectFrom([inputUtxo])
