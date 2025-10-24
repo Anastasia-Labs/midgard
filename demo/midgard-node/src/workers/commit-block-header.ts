bc8f4e324cbe6a82db7bc5d361039d94ca6d768d
Grigory Petrov <71918928+Cauterization@users.noreply.github.com>
2025-10-24 14:49:00 +0300

    feature: add tx order event to commit-block-header

--- demo/midgard-node/src/workers/commit-block-header.ts
+++ demo/midgard-node/src/workers/commit-block-header.ts
@@ -31,7 +31,6 @@ import {
   MptError,
   keyValueMptRoot,
   makeMpts,
-  processMpts,
   withTrieTransaction,
 } from "@/workers/utils/mpt.js";
 import { FileSystemError, batchProgram } from "@/utils.js";
@@ -40,6 +39,7 @@ import {
   Columns as TxColumns,
 } from "@/database/utils/tx.js";
 import { DatabaseError } from "@/database/utils/common.js";
+import { processDepositEvent, processTxOrderEvent, processTxRequestEvent } from "./utils/events.js";
 
 const BATCH_SIZE = 100;
 
@@ -87,7 +87,8 @@ const wrapper = (
 
     const mempoolTxs = yield* MempoolDB.retrieve;
     const mempoolTxsCount = mempoolTxs.length;
-    let latestTxEntry: TxEntry;
+    let latestTxEntry: undefined | TxEntry;
+    // let endTime: Date;
 
     if (mempoolTxsCount === 0) {
       yield* Effect.logInfo(
@@ -97,23 +98,26 @@ const wrapper = (
       const processedMempoolTxs = yield* ProcessedMempoolDB.retrieve;
 
       if (processedMempoolTxs.length === 0) {
-        yield* Effect.logInfo("🔹 Nothing to commit.");
-        return {
-          type: "NothingToCommitOutput",
-        } as WorkerOutput;
+        yield* Effect.logInfo("🔹 No transactions were found in ProcessedMempoolDB");
+        // endTime = new Date();
+        // return {
+        //   type: "NothingToCommitOutput",
+        // } as WorkerOutput;
       } else {
         latestTxEntry = processedMempoolTxs[0];
+        // endTime = latestTxEntry[TxColumns.TIMESTAMPTZ]
       }
       // No new transactions received, but there are uncommitted transactions in
       // the MPT. So its root must be used to submit a new block, and if
       // successful, `ProcessedMempoolDB` must be cleared. Following functions
       // should work fine with 0 mempool txs.
     } else {
-      yield* Effect.logInfo("🔹 Transactions were found in MempoolDB");
+      yield* Effect.logInfo(`🔹 ${mempoolTxsCount} transactions were found in MempoolDB`);
       latestTxEntry = mempoolTxs[0];
+      // endTime = latestTxEntry[TxColumns.TIMESTAMPTZ]
     }
 
-    yield* Effect.logInfo(`🔹 ${mempoolTxsCount} retrieved.`);
+    // yield* Effect.logInfo(`🔹 ${mempoolTxsCount} retrieved.`);
 
     const { ledgerTrie, mempoolTrie } = yield* makeMpts;
 
@@ -130,12 +134,9 @@ const wrapper = (
       | MptError,
       AlwaysSucceedsContract | Database | NodeConfig
     > = Effect.gen(function* () {
-      const { utxoRoot, txRoot, mempoolTxHashes, sizeOfProcessedTxs } =
-        yield* processMpts(ledgerTrie, mempoolTrie, mempoolTxs);
-
       const { stateQueueAuthValidator } = yield* AlwaysSucceedsContract;
 
-      const skippedSubmissionProgram = batchProgram(
+      const skippedSubmissionProgram = (mempoolTxHashes : Buffer[]) => batchProgram(
         BATCH_SIZE,
         mempoolTxsCount,
         "skipped-submission-db-transfer",
@@ -167,11 +168,15 @@ const wrapper = (
         yield* Effect.logInfo(
           "🔹 No confirmed blocks available. Transferring to ProcessedMempoolDB...",
         );
-        yield* skippedSubmissionProgram;
+        // TODO: Should we even construct mpts on that iteration? Wouldn't it lead to case
+        // when tx requests applied before tx orders?
+        const { mempoolTxHashes, sizeOfTxRequestTxs } =
+          yield* processTxRequestEvent(ledgerTrie, mempoolTrie, mempoolTxs);
+        yield* skippedSubmissionProgram(mempoolTxHashes);
         return {
           type: "SkippedSubmissionOutput",
           mempoolTxsCount,
-          sizeOfProcessedTxs,
+          sizeOfProcessedTxs: sizeOfTxRequestTxs,
         };
       } else {
         yield* Effect.logInfo(
@@ -187,30 +192,26 @@ const wrapper = (
         yield* lucid.switchToOperatorsMainWallet;
 
         const startTime = yield* getLatestBlockDatumEndTime(latestBlock.datum);
-        const endTime = latestTxEntry[TxColumns.TIMESTAMPTZ];
-        const deposits = yield* DepositsDB.retrieveTimeBoundEntries(
-          startTime,
-          endTime,
-        );
+        const endTime = latestTxEntry === undefined
+          ? new Date()
+          : latestTxEntry[TxColumns.TIMESTAMPTZ];
 
-        const depositIDs = deposits.map(
-          (deposit) => deposit[DepositsDB.Columns.ID],
-        );
-        const depositInfos = deposits.map(
-          (deposit) => deposit[DepositsDB.Columns.INFO],
-        );
+        const sizeOfTxOrderTxs = yield* processTxOrderEvent(startTime, endTime, ledgerTrie);
+        const {mempoolTxHashes, sizeOfTxRequestTxs} = yield* processTxRequestEvent(ledgerTrie, mempoolTrie, mempoolTxs)
+        const {depositRoot, sizeOfDepositTxs} = yield* processDepositEvent(startTime, endTime)
+        const sizeOfProcessedTxs = sizeOfTxOrderTxs + sizeOfTxRequestTxs + sizeOfDepositTxs;
 
-        const depositRootFiber = yield* Effect.fork(
-          keyValueMptRoot(depositIDs, depositInfos),
-        );
+        if (sizeOfProcessedTxs === 0) return  {
+          type: "NothingToCommitOutput",
+        } as WorkerOutput;;
 
         const { nodeDatum: updatedNodeDatum, header: newHeader } =
           yield* SDK.Utils.updateLatestBlocksDatumAndGetTheNewHeader(
             lucid.api,
             latestBlock.datum,
-            utxoRoot,
-            txRoot,
-            yield* depositRootFiber,
+            yield* ledgerTrie.getRoot(),
+            yield* mempoolTrie.getRoot(),
+            depositRoot,
             "00".repeat(32),
             BigInt(Number(endTime)),
           );
@@ -362,7 +363,7 @@ const wrapper = (
                 // logic as the case where no confirmed blocks are available.
                 //
                 // TODO: Handle failures properly.
-                yield* skippedSubmissionProgram;
+                yield* skippedSubmissionProgram(mempoolTxHashes);
                 return yield* failedSubmissionProgram(e);
               }),
           }),
---
+++ demo/midgard-node/src/workers/utils/events.ts
@@ -0,0 +1,158 @@
+import { TxOrdersDB, DepositsDB, LedgerUtils } from "@/database/index.js";
+import { Effect } from "effect";
+import * as ETH_UTILS from "@ethereumjs/util";
+import { findSpentAndProducedUTxOs } from "@/utils.js";
+import { Database } from "@/services/database.js";
+import { DatabaseError } from "@/database/utils/common.js";
+import * as SDK from "@al-ft/midgard-sdk";
+import { keyValueMptRoot, MidgardMpt, MptError } from "./mpt.js";
+import * as Tx from "@/database/utils/tx.js";
+import * as Ledger from "@/database/utils/ledger.js";
+
+export const processTxOrderEvent = (
+  startTime: Date,
+  endTime: Date,
+  ledgerTrie: MidgardMpt,
+): Effect.Effect<
+  number,
+  DatabaseError | SDK.Utils.CmlUnexpectedError | MptError,
+  Database
+> =>
+  Effect.gen(function* () {
+    const txOrders = yield* TxOrdersDB.retrieveTimeBoundEntries(
+      startTime,
+      endTime,
+    );
+    const utxoBatchDBOps: ETH_UTILS.BatchDBOp[] = [];
+    yield* Effect.logInfo(`🔹 Processing ${txOrders.length} new tx orders...`);
+    let sizeOfProcessedTxs = 0;
+    yield* Effect.forEach(txOrders, (entry: TxOrdersDB.Entry) =>
+      Effect.gen(function* () {
+        const txHash = entry[TxOrdersDB.Columns.ID];
+        const txCbor = entry[TxOrdersDB.Columns.INFO];
+        const { spent, produced } = yield* findSpentAndProducedUTxOs(
+          txCbor,
+          txHash,
+        ).pipe(Effect.withSpan("findSpentAndProducedUTxOs"));
+        sizeOfProcessedTxs += txCbor.length;
+        const delOps: ETH_UTILS.BatchDBOp[] = spent.map((outRef) => ({
+          type: "del",
+          key: outRef,
+        }));
+        const putOps: ETH_UTILS.BatchDBOp[] = produced.map(
+          (le: LedgerUtils.MinimalEntry) => ({
+            type: "put",
+            key: le[LedgerUtils.Columns.OUTREF],
+            value: le[LedgerUtils.Columns.OUTPUT],
+          }),
+        );
+        yield* Effect.sync(() => utxoBatchDBOps.push(...delOps));
+        yield* Effect.sync(() => utxoBatchDBOps.push(...putOps));
+      }),
+    );
+    yield* ledgerTrie.batch(utxoBatchDBOps)
+    const utxoRoot = yield* ledgerTrie.getRootHex();
+    yield* Effect.logInfo(`🔹 ${txOrders.length} new tx orders processed - new ledger root is ${utxoRoot}`);
+    yield* Effect.logInfo(`🔹 New UTxO root found: ${utxoRoot}`);
+    return sizeOfProcessedTxs;
+  });
+
+export const processTxRequestEvent = (
+  ledgerTrie: MidgardMpt,
+  mempoolTrie: MidgardMpt,
+  mempoolTxs: readonly Tx.Entry[],
+): Effect.Effect<
+  {
+    mempoolTxHashes: Buffer[];
+    sizeOfTxRequestTxs: number;
+  },
+  MptError | SDK.Utils.CmlUnexpectedError,
+  Database
+> =>
+  Effect.gen(function* () {
+    const mempoolTxHashes: Buffer[] = [];
+    const mempoolBatchOps: ETH_UTILS.BatchDBOp[] = [];
+    const batchDBOps: ETH_UTILS.BatchDBOp[] = [];
+    let sizeOfProcessedTxs = 0;
+    yield* Effect.logInfo(`🔹 Processing ${mempoolTxs.length} new tx requests...`);
+    yield* Effect.forEach(mempoolTxs, (entry: Tx.Entry) =>
+      Effect.gen(function* () {
+        const txHash = entry[Tx.Columns.TX_ID];
+        const txCbor = entry[Tx.Columns.TX];
+        mempoolTxHashes.push(txHash);
+        const { spent, produced } = yield* findSpentAndProducedUTxOs(
+          txCbor,
+          txHash,
+        ).pipe(Effect.withSpan("findSpentAndProducedUTxOs"));
+        sizeOfProcessedTxs += txCbor.length;
+        const delOps: ETH_UTILS.BatchDBOp[] = spent.map((outRef) => ({
+          type: "del",
+          key: outRef,
+        }));
+        const putOps: ETH_UTILS.BatchDBOp[] = produced.map(
+          (le: Ledger.MinimalEntry) => ({
+            type: "put",
+            key: le[Ledger.Columns.OUTREF],
+            value: le[Ledger.Columns.OUTPUT],
+          }),
+        );
+        yield* Effect.sync(() =>
+          mempoolBatchOps.push({
+            type: "put",
+            key: txHash,
+            value: txCbor,
+          }),
+        );
+        yield* Effect.sync(() => batchDBOps.push(...delOps));
+        yield* Effect.sync(() => batchDBOps.push(...putOps));
+      }),
+    );
+
+    yield* Effect.all(
+      [ mempoolTrie.batch(mempoolBatchOps)
+      , ledgerTrie.batch(batchDBOps)
+      ],
+      { concurrency: "unbounded" },
+    );
+
+    const txRoot = yield* mempoolTrie.getRootHex();
+    const utxoRoot = yield* ledgerTrie.getRootHex();
+    yield* Effect.logInfo(`🔹 ${mempoolTxs.length} new tx requests processed`);
+    yield* Effect.logInfo(`🔹 New transaction root found: ${txRoot}`);
+    yield* Effect.logInfo(`🔹 New UTxO root found: ${utxoRoot}`);
+
+    return {
+      mempoolTxHashes,
+      sizeOfTxRequestTxs: sizeOfProcessedTxs,
+    };
+  });
+
+export const processDepositEvent = (
+  startTime: Date,
+  endTime: Date,
+): Effect.Effect<{depositRoot: string, sizeOfDepositTxs: number}, DatabaseError | MptError, Database> => Effect.gen(function* () {
+  const deposits = yield* DepositsDB.retrieveTimeBoundEntries(
+    startTime,
+    endTime,
+  );
+  yield* Effect.logInfo(`🔹 Processing ${deposits.length} new deposits...`);
+  const depositIDs = deposits.map(
+    (deposit) => deposit[DepositsDB.Columns.ID],
+  );
+  const depositInfos = deposits.map(
+    (deposit) => deposit[DepositsDB.Columns.INFO],
+  );
+  const depositRootFiber = yield* Effect.fork(
+    keyValueMptRoot(depositIDs, depositInfos),
+  );
+  const depositRoot = yield* depositRootFiber;
+  const sizeOfDepositTxs = depositInfos
+      .map(i => i.length)
+      .reduce((acc, curr) => acc + curr, 0);
+  yield* Effect.logInfo(`🔹 ${deposits.length} new deposits processed - new deposit root is ${depositRoot}`);
+  yield* Effect.logInfo(`🔹 New deposits root found: ${depositRoot}`);
+  return {
+    depositRoot,
+    sizeOfDepositTxs,
+};
+})
