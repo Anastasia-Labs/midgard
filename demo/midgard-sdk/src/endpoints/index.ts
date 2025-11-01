e57a172a002d60e77a1f7dd02f652a0ae42701cf
Grigory Petrov <71918928+Cauterization@users.noreply.github.com>
2025-11-01 14:05:13 +0300

    refactor: clean imports

--- demo/midgard-sdk/src/endpoints/index.ts
+++
@@ -1,2 +0,0 @@
-export * as HubOracle from "./hub-oracle/index.js";
-export * as UserEvents from "./user-events/index.js";
--- demo/midgard-sdk/src/index.ts
+++ demo/midgard-sdk/src/index.ts
@@ -2,7 +2,6 @@ export * from "./user-events/deposit.js";
 export * from "./consensus/state-queue.js";
 export * from "./consensus/hub-oracle.js";
 
-export * as Endpoints from "./endpoints/index.js";
 export * as Utils from "./utils/index.js";
 export * as TxBuilder from "./tx-builder/index.js";
 export * from "./protocolParameters.js";
