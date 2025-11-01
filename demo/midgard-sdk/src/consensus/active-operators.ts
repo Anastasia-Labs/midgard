222e38d6c6c4d7d0f4c8400d2a5cd5fe460d24c9
Grigory Petrov <71918928+Cauterization@users.noreply.github.com>
2025-11-01 14:26:37 +0300

    refactor: active operator

---
+++ demo/midgard-sdk/src/consensus/active-operators.ts
@@ -0,0 +1,177 @@
+import { POSIXTimeSchema } from "@/tx-builder/common.js";
+import { LucidEvolution, TxBuilder } from "@lucid-evolution/lucid";
+import { Data } from "@lucid-evolution/lucid";
+
+export const ActiveOperatorDatumSchema = Data.Object({
+  commitmentTime: Data.Nullable(POSIXTimeSchema),
+});
+export type ActiveOperatorDatum = Data.Static<typeof ActiveOperatorDatumSchema>;
+export const ActiveOperatorDatum =
+  ActiveOperatorDatumSchema as unknown as ActiveOperatorDatum;
+
+export const ActiveOperatorSpendRedeemerSchema = Data.Enum([
+  Data.Literal("ListStateTransition"),
+  Data.Object({
+    UpdateCommitmentTime: Data.Object({
+      activeNodeInputIndex: Data.Integer(),
+      prevActiveNodeRefInputIndex: Data.Integer(),
+      hubOracleRefInputIndex: Data.Integer(),
+      stateQueueRedeemerIndex: Data.Integer(),
+    }),
+  }),
+]);
+export type ActiveOperatorSpendRedeemer = Data.Static<
+  typeof ActiveOperatorSpendRedeemerSchema
+>;
+export const ActiveOperatorSpendRedeemer =
+  ActiveOperatorSpendRedeemerSchema as unknown as ActiveOperatorSpendRedeemer;
+
+export const ActiveOperatorMintRedeemerSchema = Data.Enum([
+  Data.Literal("Init"),
+  Data.Literal("Deinit"),
+  Data.Object({
+    ActivateOperator: Data.Object({
+      newActiveOperatorKey: Data.Bytes(),
+      hubOracleRefInputIndex: Data.Integer(),
+      activeOperatorAppendedNodeOutputIndex: Data.Integer(),
+      activeOperatorAnchorNodeOutputIndex: Data.Integer(),
+      registeredOperatorsRedeemerIndex: Data.Integer(),
+    }),
+  }),
+  Data.Object({
+    RemoveOperatorSlashBond: Data.Object({
+      slashedActiveOperatorKey: Data.Bytes(),
+      hubOracleRefInputIndex: Data.Integer(),
+      activeOperatorSlashedNodeInputIndex: Data.Integer(),
+      activeOperatorAnchorNodeInputIndex: Data.Integer(),
+      stateQueueRedeemerIndex: Data.Integer(),
+    }),
+  }),
+  Data.Object({
+    RetireOperator: Data.Object({
+      activeOperatorKey: Data.Bytes(),
+      hubOracleRefInputIndex: Data.Integer(),
+      activeOperatorRemovedNodeInputIndex: Data.Integer(),
+      activeOperatorAnchorNodeInputIndex: Data.Integer(),
+      retiredOperatorInsertedNodeOutputIndex: Data.Integer(),
+      retiredOperatorsRedeemerIndex: Data.Integer(),
+    }),
+  }),
+]);
+export type ActiveOperatorMintRedeemer = Data.Static<
+  typeof ActiveOperatorMintRedeemerSchema
+>;
+export const MintRedeemer =
+  ActiveOperatorMintRedeemerSchema as unknown as ActiveOperatorMintRedeemer;
+
+export type ActiveOperatorInitParams = {};
+export type ActiveOperatorDeinitParams = {};
+export type ActiveOperatorActivateParams = {};
+export type ActiveOperatorRetireParams = {};
+export type ActiveOperatorListStateTransitionParams = {};
+export type ActiveOperatorRemoveSlashBondParams = {};
+export type ActiveOperatorUpdateCommitmentTimeParams = {};
+
+/**
+ * Init
+ *
+ * @param lucid - The LucidEvolution
+ * @param params - The parameters
+ * @returns {TxBuilder} A TxBuilder instance that can be used to build the transaction.
+ */
+export const incompleteActiveOperatorInitTxProgram = (
+  lucid: LucidEvolution,
+  params: ActiveOperatorInitParams,
+): TxBuilder => {
+  const tx = lucid.newTx();
+  return tx;
+};
+
+/**
+ * The program that performs the deinit of an operator.
+ *
+ * @param lucid - The LucidEvolution instance to use for the deinit.
+ * @param params - The parameters required for deinit.
+ * @returns {TxBuilder} A TxBuilder instance that can be used to build the transaction.
+ */
+export const incompleteActiveOperatorDeinitTxProgram = (
+  lucid: LucidEvolution,
+  params: ActiveOperatorDeinitParams,
+): TxBuilder => {
+  const tx = lucid.newTx();
+  return tx;
+};
+/**
+ * The program that performs the activation of an operator.
+ *
+ * @param lucid - The LucidEvolution instance to use for the activation.
+ * @param params - The parameters required for activation.
+ * @returns {TxBuilder} A TxBuilder instance that can be used to build the transaction.
+ */
+export const incompleteActiveOperatorActivateTxProgram = (
+  lucid: LucidEvolution,
+  params: ActiveOperatorActivateParams,
+): TxBuilder => {
+  const tx = lucid.newTx();
+  return tx;
+};
+
+/**
+ * Retire
+ *
+ * @param lucid - The LucidEvolution
+ * @param params - The parameters
+ * @returns {TxBuilder} A TxBuilder instance that can be used to build the transaction.
+ */
+export const incompleteActiveOperatorRetireTxProgram = (
+  lucid: LucidEvolution,
+  params: ActiveOperatorRetireParams,
+): TxBuilder => {
+  const tx = lucid.newTx();
+  return tx;
+};
+
+/**
+ * ListStateTransition
+ *
+ * @param lucid - The LucidEvolution
+ * @param params - The parameters
+ * @returns {TxBuilder} A TxBuilder instance that can be used to build the transaction.
+ */
+export const incompleteActiveOperatorListStateTransitionTxProgram = (
+  lucid: LucidEvolution,
+  params: ActiveOperatorListStateTransitionParams,
+): TxBuilder => {
+  const tx = lucid.newTx();
+  return tx;
+};
+
+/**
+ * Remove slash bond
+ *
+ * @param lucid - The LucidEvolution
+ * @param params - The parameters
+ * @returns {TxBuilder} A TxBuilder instance that can be used to build the transaction.
+ */
+export const incompleteActiveOperatorRemoveSlashBondTxProgram = (
+  lucid: LucidEvolution,
+  params: ActiveOperatorRemoveSlashBondParams,
+): TxBuilder => {
+  const tx = lucid.newTx();
+  return tx;
+};
+
+/**
+ * UpdateCommitmentTime
+ *
+ * @param lucid - The LucidEvolution
+ * @param params - The parameters
+ * @returns {TxBuilder} A TxBuilder instance that can be used to build the transaction.
+ */
+export const incompleteActiveOperatorUpdateCommitmentTimeTxProgram = (
+  lucid: LucidEvolution,
+  params: ActiveOperatorUpdateCommitmentTimeParams,
+): TxBuilder => {
+  const tx = lucid.newTx();
+  return tx;
+};
