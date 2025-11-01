f98cab18b071707ee5a70c40c01e2ec9158d3cc3
Grigory Petrov <71918928+Cauterization@users.noreply.github.com>
2025-11-01 14:42:44 +0300

    refactor: registered operators

--- demo/midgard-sdk/src/index.ts
+++ demo/midgard-sdk/src/index.ts
@@ -1,6 +1,8 @@
 export * from "./user-events/deposit.js";
 export * from "./consensus/state-queue.js";
 export * from "./consensus/hub-oracle.js";
+export * from "./consensus/active-operators.js";
+export * from "./consensus/registered-operators.js";
 
 export * as Utils from "./utils/index.js";
 export * as TxBuilder from "./tx-builder/index.js";
