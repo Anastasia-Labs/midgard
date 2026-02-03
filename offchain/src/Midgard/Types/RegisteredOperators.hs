f79ff1e1cda1b6c8fe8bbdbb66614987b3dfcdf7
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2026-02-03 13:56:52 -0700

    Initialize offchain project

---
+++ offchain/src/Midgard/Types/RegisteredOperators.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Midgard.Types.RegisteredOperators (OperatorStatus, MintRedeemer (..)) where
+
+import GHC.Generics
+
+import PlutusLedgerApi.V3
+import PlutusTx.Blueprint (HasBlueprintDefinition, definitionRef)
+import PlutusTx.Blueprint.TH (makeIsDataSchemaIndexed)
+import Ply (PlyArg)
+
+data OperatorStatus
+  = Registered
+  | Active
+  | Retired
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (HasBlueprintDefinition)
+
+$(makeIsDataSchemaIndexed ''OperatorStatus [('Registered, 0), ('Active, 1), ('Retired, 2)])
+
+data MintRedeemer
+  = Init
+  | Deinit
+  | Register
+      { keyToPrepend :: BuiltinByteString
+      , hubOracleRefInputIndex :: Integer
+      , activeOperatorRefInputIndex :: Integer
+      , activeOperatorAssetName :: BuiltinByteString
+      , retiredOperatorRefInputIndex :: Integer
+      , retiredOperatorAssetName :: BuiltinByteString
+      , prependedNodeOutputIndex :: Integer
+      , anchorNodeOutputIndex :: Integer
+      }
+  | Activate
+      { nodeToActivateKey :: BuiltinByteString
+      , hubOracleRefInputIndex :: Integer
+      , retiredOperatorRefInputIndex :: Integer
+      , retiredOperatorAssetName :: BuiltinByteString
+      , removedNodeInputIndex :: Integer
+      , anchorNodeInputIndex :: Integer
+      , activeOperatorsInsertedNodeOutputIndex :: Integer
+      , activeOperatorsAnchorNodeOutputIndex :: Integer
+      }
+  | Deregister
+      { nodeToDeregisterKey :: BuiltinByteString
+      , removedNodeInputIndex :: Integer
+      , anchorNodeInputIndex :: Integer
+      }
+  | RemoveDuplicateSlashBond
+      { duplicateNodeKey :: BuiltinByteString
+      , hubOracleRefInputIndex :: Integer
+      , duplicateNodeRefInputIndex :: Integer
+      , duplicateNodeRefInputAssetName :: BuiltinByteString
+      , removedNodeInputIndex :: Integer
+      , anchorNodeInputIndex :: Integer
+      , witnessStatus :: OperatorStatus
+      }
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (HasBlueprintDefinition)
+
+-- Make sure to use PlutusTx's tools for implementing data and blueprint instances for our custom type!
+$( makeIsDataSchemaIndexed
+     ''MintRedeemer
+     [ ('Init, 0)
+     , ('Deinit, 1)
+     , ('Register, 2)
+     , ('Activate, 3)
+     , ('Deregister, 4)
+     , ('RemoveDuplicateSlashBond, 5)
+     ]
+ )
+
+instance PlyArg MintRedeemer
