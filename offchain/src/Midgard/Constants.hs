8d1a742ac05ebfa0bf75520116419cb2cb51e849
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2026-02-08 17:12:10 -0700

    PlutusV3 alwaysSucceeds script

--- offchain/src/Midgard/Constants.hs
+++ offchain/src/Midgard/Constants.hs
@@ -8,9 +8,12 @@ module Midgard.Constants (
 
 import Cardano.Api qualified as C
 import Convex.Utils.String (unsafeAssetName)
+import PlutusCore.Core qualified as PLC
+import PlutusLedgerApi.V3 (serialiseUPLC)
+import UntypedPlutusCore qualified as UPLC
 
-hubOracleMintingScript :: C.PlutusScript C.PlutusScriptV1
-hubOracleMintingScript = C.examplePlutusScriptAlwaysSucceeds C.WitCtxMint
+hubOracleMintingScript :: C.PlutusScript C.PlutusScriptV3
+hubOracleMintingScript = C.PlutusScriptSerialised $ serialiseUPLC alwaysSucceedsUPLC
 
 hubOracleMintingPolicyId :: C.PolicyId
 hubOracleMintingPolicyId = C.PolicyId . C.hashScript $ C.PlutusScript C.plutusScriptVersion hubOracleMintingScript
@@ -23,3 +26,9 @@ operatorRequiredBond = 5_000_000
 
 operatorSlashingPenalty :: C.Lovelace
 operatorSlashingPenalty = 3_000_000
+
+alwaysSucceedsUPLC :: UPLC.Program UPLC.DeBruijn uni fun ()
+alwaysSucceedsUPLC =
+  UPLC.Program () PLC.plcVersion110 $
+    UPLC.LamAbs () (UPLC.DeBruijn 0) $
+      UPLC.Var () (UPLC.DeBruijn 1)
