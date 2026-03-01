311f717237ff15e3e8d597a7362648ef715456e8
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2026-03-01 12:26:50 -0700

    Add initProtocol test

--- offchain/tests/Spec.hs
+++ offchain/tests/Spec.hs
@@ -1,7 +1,12 @@
 import Test.Tasty
 
-tests :: TestTree
-tests = testGroup "tests" []
+import Midgard.Scripts (MidgardScripts, readAikenScripts)
+import Spec.RegisterOperator qualified as RegisterOperator
+
+tests :: MidgardScripts -> TestTree
+tests ms = testGroup "tests" [RegisterOperator.tests ms]
 
 main :: IO ()
-main = defaultMain tests
+main = do
+  ms <- readAikenScripts
+  defaultMain $ tests ms
