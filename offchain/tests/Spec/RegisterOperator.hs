91cc759cc47ae7a80deb0bd8748ef084d28c398d
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2026-03-01 12:26:50 -0700

    Add initProtocol test

--- offchain/tests/Spec/RegisterOperator.hs
+++ offchain/tests/Spec/RegisterOperator.hs
@@ -1,5 +1,28 @@
 module Spec.RegisterOperator (tests) where
 
+import Cardano.Api qualified as C
+import Convex.BuildTx (execBuildTx, setMinAdaDepositAll)
+import Convex.Class (MonadBlockchain (queryProtocolParameters))
+import Convex.CoinSelection (ChangeOutputPosition (TrailingChange))
+import Convex.MockChain.CoinSelection
+import Convex.MockChain.Defaults qualified as Defaults
+import Convex.MockChain.Utils (mockchainSucceeds)
+import Convex.Utils (failOnError)
+import Convex.Wallet.MockWallet qualified as Wallet
+import Midgard.Contracts.Init (initProtocol)
+import Midgard.Scripts (MidgardScripts)
 import Test.Tasty
+import Test.Tasty.HUnit (testCase)
 
-tests = testGroup "register-operator" []
+tests :: MidgardScripts -> TestTree
+tests ms =
+  testGroup
+    "register-operator"
+    [ testCase "initProtocol" . mockchainSucceeds . failOnError $ do
+        let netId = Defaults.networkId
+        params <- queryProtocolParameters
+        let txBody = execBuildTx $ do
+              setMinAdaDepositAll params
+              initProtocol @C.ConwayEra netId ms
+        balanceAndSubmit mempty Wallet.w1 txBody TrailingChange []
+    ]
