94e057d4bb73a6dec644e7137caf2fbb859786b7
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2026-03-01 15:32:49 -0700

    Hoist out initProtocol as a utility helper

--- offchain/tests/Spec/RegisterOperator.hs
+++ offchain/tests/Spec/RegisterOperator.hs
@@ -1,28 +1,14 @@
 module Spec.RegisterOperator (tests) where
 
-import Cardano.Api qualified as C
-import Convex.BuildTx (execBuildTx, setMinAdaDepositAll)
-import Convex.Class (MonadBlockchain (queryProtocolParameters))
-import Convex.CoinSelection (ChangeOutputPosition (TrailingChange))
-import Convex.MockChain.CoinSelection
-import Convex.MockChain.Defaults qualified as Defaults
-import Convex.MockChain.Utils (mockchainSucceeds)
-import Convex.Utils (failOnError)
-import Convex.Wallet.MockWallet qualified as Wallet
-import Midgard.Contracts.Init (initProtocol)
 import Midgard.Scripts (MidgardScripts)
 import Test.Tasty
-import Test.Tasty.HUnit (testCase)
+
+import Spec.Utils (midgardTestCase)
 
 tests :: MidgardScripts -> TestTree
 tests ms =
   testGroup
     "register-operator"
-    [ testCase "initProtocol" . mockchainSucceeds . failOnError $ do
-        let netId = Defaults.networkId
-        params <- queryProtocolParameters
-        let txBody = execBuildTx $ do
-              setMinAdaDepositAll params
-              initProtocol @C.ConwayEra netId ms
-        balanceAndSubmit mempty Wallet.w1 txBody TrailingChange []
+    [ midgardTestCase @String ms "register-operator" $ do
+        pure ()
     ]
