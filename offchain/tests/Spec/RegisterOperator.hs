421e76eb2b1b511ce8ea144c2251f6090331533a
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2026-03-02 13:53:49 -0700

    Add initProtocol test scaffold

--- offchain/tests/Spec/RegisterOperator.hs
+++ offchain/tests/Spec/RegisterOperator.hs
@@ -3,12 +3,20 @@ module Spec.RegisterOperator (tests) where
 import Midgard.Scripts (MidgardScripts)
 import Test.Tasty
 
-import Spec.Utils (midgardTestCase)
+import Cardano.Api qualified as C
+import Control.Monad.Except (withExceptT)
+import Convex.CoinSelection (ChangeOutputPosition (TrailingChange))
+import Convex.Wallet qualified as Wallet
+import Convex.Wallet.MockWallet qualified as Wallet
+import Data.Functor (void)
+import Midgard.Contracts.RegisteredOperators
+import Spec.Types (TestTxError (TxBuildingError))
+import Spec.Utils (balanceAndSubmit', midgardTestCase)
 
 tests :: MidgardScripts -> TestTree
 tests ms =
   testGroup
     "register-operator"
-    [ midgardTestCase @String ms "register-operator" $ do
+    [ midgardTestCase ms "register-operator" $ \refScripts -> do
         pure ()
     ]
