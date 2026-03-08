cb65724147e41b4690a1585e62a7e58f33f78172
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2026-03-08 16:44:39 -0600

    Add register operator happy path test

--- offchain/tests/Spec/RegisterOperator.hs
+++ offchain/tests/Spec/RegisterOperator.hs
@@ -1,15 +1,16 @@
 module Spec.RegisterOperator (tests) where
 
-import Midgard.Scripts (MidgardScripts)
-import Test.Tasty
-
-import Cardano.Api qualified as C
 import Control.Monad.Except (withExceptT)
+import Data.Functor (void)
+
 import Convex.CoinSelection (ChangeOutputPosition (TrailingChange))
 import Convex.Wallet qualified as Wallet
 import Convex.Wallet.MockWallet qualified as Wallet
-import Data.Functor (void)
+import Test.Tasty
+
 import Midgard.Contracts.RegisteredOperators
+import Midgard.Scripts (MidgardScripts)
+
 import Spec.Types (TestTxError (TxBuildingError))
 import Spec.Utils (balanceAndSubmit', midgardTestCase)
 
@@ -18,5 +19,8 @@ tests ms =
   testGroup
     "register-operator"
     [ midgardTestCase ms "register-operator" $ \refScripts -> do
-        pure ()
+        let operatorWallet = Wallet.w1
+            operatorPkh = Wallet.verificationKeyHash operatorWallet
+        txBody <- withExceptT TxBuildingError $ registerOperator ms refScripts operatorPkh
+        void $ balanceAndSubmit' operatorWallet txBody TrailingChange []
     ]
