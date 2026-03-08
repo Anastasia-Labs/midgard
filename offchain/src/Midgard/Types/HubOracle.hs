f0fe282160cd43bde7c5cb1bb95a9c7d75434e46
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2026-03-08 16:08:32 -0600

    Declare full hub datum type

--- offchain/src/Midgard/Types/HubOracle.hs
+++ offchain/src/Midgard/Types/HubOracle.hs
@@ -4,7 +4,7 @@ module Midgard.Types.HubOracle (Datum (..)) where
 
 import GHC.Generics (Generic)
 
-import PlutusLedgerApi.V3 (CurrencySymbol)
+import PlutusLedgerApi.V3 (Address, CurrencySymbol, ScriptHash)
 import PlutusTx.Blueprint (HasBlueprintDefinition, definitionRef)
 import PlutusTx.Blueprint.TH (makeIsDataSchemaIndexed)
 
@@ -12,29 +12,29 @@ data Datum = Datum
   { registeredOperators :: CurrencySymbol
   , activeOperators :: CurrencySymbol
   , retiredOperators :: CurrencySymbol
-  --   , scheduler                  :: CurrencySymbol
-  --   , stateQueue                 :: CurrencySymbol
-  --   , fraudProofCatalogue        :: CurrencySymbol
-  --   , fraudProof                 :: CurrencySymbol
-  --   , deposit                    :: CurrencySymbol
-  --   , withdrawal                 :: CurrencySymbol
-  --   , txOrder                    :: CurrencySymbol
-  --   , settlement                 :: CurrencySymbol
-  --   , payout                     :: CurrencySymbol
-  --   , registeredOperatorsAddr    :: Address
-  --   , activeOperatorsAddr        :: Address
-  --   , retiredOperatorsAddr       :: Address
-  --   , schedulerAddr              :: Address
-  --   , stateQueueAddr             :: Address
-  --   , fraudProofCatalogueAddr    :: Address
-  --   , fraudProofAddr             :: Address
-  --   , depositAddr                :: Address
-  --   , withdrawalAddr             :: Address
-  --   , txOrderAddr                :: Address
-  --   , settlementAddr             :: Address
-  --   , reserveAddr                :: Address
-  --   , payoutAddr                 :: Address
-  --   , reserveObserver            :: ScriptHash
+  , scheduler :: CurrencySymbol
+  , stateQueue :: CurrencySymbol
+  , fraudProofCatalogue :: CurrencySymbol
+  , fraudProof :: CurrencySymbol
+  , deposit :: CurrencySymbol
+  , withdrawal :: CurrencySymbol
+  , txOrder :: CurrencySymbol
+  , settlement :: CurrencySymbol
+  , payout :: CurrencySymbol
+  , registeredOperatorsAddr :: Address
+  , activeOperatorsAddr :: Address
+  , retiredOperatorsAddr :: Address
+  , schedulerAddr :: Address
+  , stateQueueAddr :: Address
+  , fraudProofCatalogueAddr :: Address
+  , fraudProofAddr :: Address
+  , depositAddr :: Address
+  , withdrawalAddr :: Address
+  , txOrderAddr :: Address
+  , settlementAddr :: Address
+  , reserveAddr :: Address
+  , payoutAddr :: Address
+  , reserveObserver :: ScriptHash
   }
   deriving stock (Eq, Show, Generic)
   deriving anyclass (HasBlueprintDefinition)
