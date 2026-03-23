75f58df4a2ec1c64fa84f3735bf6d985aa5cc5b3
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2026-03-22 18:50:55 -0600

    Add scheduler tests

--- offchain/tests/Spec.hs
+++ offchain/tests/Spec.hs
@@ -3,9 +3,10 @@ import Test.Tasty
 import Midgard.Scripts (MidgardScripts, readAikenScripts)
 
 import Spec.Operators qualified as Operators
+import Spec.Scheduler qualified as Scheduler
 
 tests :: MidgardScripts -> TestTree
-tests ms = testGroup "tests" [Operators.tests ms]
+tests ms = testGroup "tests" [Operators.tests ms, Scheduler.tests ms]
 
 main :: IO ()
 main = do
