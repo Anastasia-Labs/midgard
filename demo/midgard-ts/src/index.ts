d4f0c1f9c492ca5b3bc0ccfde5423cccab6d2436
keyanmaskoot <keyanmaskoot@gmail.com>
2026-06-11 20:47:33 +0200

    fix(demo): remove artifacts

--- demo/midgard-ts/src/index.ts
+++
@@ -1,9 +0,0 @@
-// Core codec utilities
-export * from "./codec";
-
-// Types and codecs
-export * from "./types/primitives";
-export * from "./types/output";
-export * from "./types/events";
-export * from "./types/transaction";
-export * from "./types/block";
